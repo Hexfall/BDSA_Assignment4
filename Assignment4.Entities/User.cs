8f32904afd1e83235263fe2d2dcf0dcd8749962b
Viktor Máni Jensson <Viktor.mani@hotmail.co.uk>
2021-10-01 12:46:36 +0200

    Implemented User

--- Assignment4.Entities/User.cs
+++ Assignment4.Entities/User.cs
@@ -1,6 +1,17 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
 namespace Assignment4.Entities
 {
     public class User
     {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+        [EmailAddress]
+        [Required]
+        public string Email { get; set; }
+        public IEnumerable<Task> Tasks { get; set; }
     }
 }
