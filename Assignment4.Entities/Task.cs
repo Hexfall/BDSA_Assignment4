51c6ab7e1f83505f3a39f7f9bba41caee37c45ae
Viktor Máni Jensson <Viktor.mani@hotmail.co.uk>
2021-10-01 12:42:08 +0200

    Implemented Task class.

--- Assignment4.Entities/Task.cs
+++ Assignment4.Entities/Task.cs
@@ -1,6 +1,19 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
 namespace Assignment4.Entities
 {
     public class Task
     {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+        public int? UserId { get; set; }
+        public User AssignedTo { get; set; }
+        public string Description { get; set; }
+        [Required]
+        public Core.State State { get; set; }
+        public IEnumerable<Tag> Tags { get; set; }
     }
 }
